3886f0930dfde3fa97466fb85e9eee809394ab5b
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-09-06 16:09:14 -0700

    Divisible by two done

--- routes_controllers_views_params/app/controllers/main_controller.rb
+++ routes_controllers_views_params/app/controllers/main_controller.rb
@@ -12,4 +12,14 @@ class MainController < ApplicationController
         @food2 = 'cheeseburgers'
         @food3 = 'steak'
     end
+    def cubed 
+        @result = params[:number].to_i ** 3 
+    end
+    def divtwo
+        if params[:number1].to_i % params[:number2].to_i == 0
+            @result =  "#{params[:number1]} is evenly divisible by #{params[:number2]}"
+        else 
+            @result =  "#{params[:number1]} is not evenly divisible by #{params[:number2]}"
+        end
+    end
 end
--- routes_controllers_views_params/config/routes.rb
+++ routes_controllers_views_params/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'main#home'
   get '/vanessa' => 'main#vanessa'
   get '/cody' => 'main#cody'
+  get '/cubed/:number' => 'main#cubed'
+  get '/divtwo/:number1/:number2' => 'main#divtwo'
 end
